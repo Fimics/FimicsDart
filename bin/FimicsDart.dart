09b5a0ae6bfad16a975cd405f4132507f18eaf35
pengju.li <lpjhblpj@163.com>
2021-03-16 15:46:34 +0800

    application

--- bin/FimicsDart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:FimicsDart/FimicsDart.dart' as FimicsDart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${FimicsDart.calculate()}!');
-}
--- test/FimicsDart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:FimicsDart/FimicsDart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
